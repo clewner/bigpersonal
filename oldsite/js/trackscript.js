c97992a1ff88159b0bc05b3e723b509cb6c48690
Isaac Saxonov <isaac.saxonov@gmail.com>
2025-01-12 15:41:37 -0500

    deleted

--- oldsite/js/trackscript.js
+++
@@ -1,8 +0,0 @@
-var width = $(window).width();
-$(window).on('resize', function() {
-  if ($(this).width() <= 775) {
-    $("#pigentitle").html("Pigeon<br>holed");
-  }else{
-    $("#pigentitle").html("Pigeonholed");
-  }
-});
